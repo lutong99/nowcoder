fc2c180840fa8da91c21cd8d62485756bedb61a9
lutong99 <lutong99@aliyun.com>
2023-08-10 23:58:29 +0800

    异地开发换库操作，开发完成首页的分页功能

--- src/main/java/org/example/nowcoder/controller/HomeController.java
+++ src/main/java/org/example/nowcoder/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.example.nowcoder.controller;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.example.nowcoder.entity.DiscussPost;
 import org.example.nowcoder.entity.User;
 import org.example.nowcoder.service.DiscussPostService;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -37,8 +39,11 @@ public class HomeController {
     }
 
     @GetMapping("/index")
-    public String indexPage(Model model) {
-        PageHelper.startPage(1, 10);
+    public String indexPage(Model model,
+                            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize
+    ) {
+        Page<DiscussPost> page = PageHelper.startPage(pageNum, pageSize);
         List<DiscussPost> discussPostList = discussPostService.getAll();
         List<Map<String, Object>> discussPostMapList = new ArrayList<>();
         if (discussPostList != null) {
@@ -51,7 +56,9 @@ public class HomeController {
             }
         }
 
+        PageInfo<DiscussPost> pageInfo = new PageInfo<>(page);
         model.addAttribute("discussPostList", discussPostMapList);
+        model.addAttribute("page", pageInfo);
         return "/index";
     }
 
